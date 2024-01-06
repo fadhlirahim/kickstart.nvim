fe3b5f37660815bccf4bc9ded13329e3104c9fff
Fadhli Rahim <fadhlirahim@gmail.com>
2024-01-06 13:58:52 +0800

    remove irrelevant comment

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,6 +1,3 @@
-
--- Use your favorite package manager to install, for example in lazy.nvim
---  Optionally, you can also install nvim-telescope/telescope.nvim to use some search functionality.
 return {
   {
     "github/copilot.vim",
