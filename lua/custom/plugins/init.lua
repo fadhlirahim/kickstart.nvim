0db881cc32b5c161399321bff465e3997a50716c
Fadhli Rahim <fadhlirahim@gmail.com>
2024-01-16 23:58:05 +0800

    Remove trailing whitespace on save

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -62,5 +62,14 @@ lspconfig.emmet_ls.setup({
     }
 })
 
+-- remove trailing whitespace
+vim.api.nvim_create_autocmd('BufWritePre', {
+    pattern = {"*"},
+    callback = function()
+      local save_cursor = vim.fn.getpos(".")
+      pcall(function() vim.cmd [[%s/\s\+$//e]] end)
+      vim.fn.setpos(".", save_cursor)
+    end,
+})
 
 return {}
