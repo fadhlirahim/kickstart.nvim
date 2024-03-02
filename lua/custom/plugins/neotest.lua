b84324954102bd35f0ae113eed311ebe9985d0df
Fadhli Rahim <fadhlirahim@gmail.com>
2024-03-02 10:26:20 +0800

    trying out neotest

---
+++ lua/custom/plugins/neotest.lua
@@ -0,0 +1,33 @@
+return {
+    "nvim-neotest/neotest",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "antoinemadec/FixCursorHold.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-neotest/neotest-go",
+    },
+    config = function()
+        -- get neotest namespace (api call creates or returns namespace)
+        local neotest_ns = vim.api.nvim_create_namespace("neotest")
+        vim.diagnostic.config({
+            virtual_text = {
+                format = function(diagnostic)
+                    local message =
+                    diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+                    return message
+                end,
+            },
+        }, neotest_ns)
+        require("neotest").setup({
+            experimental = {
+                test_table = true,
+            },
+            adapters = {
+                require("neotest-go")({
+                    recursive_run = true
+                }),
+            },
+        })
+    end,
+}
+
